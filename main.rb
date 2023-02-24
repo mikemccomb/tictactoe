ba6c2a32dee9d2b221d6e54a7822ac1036080674
mikemccomb <mikemccomb@gmail.com>
2023-02-23 19:51:05 -0600

    main.rb init

---
+++ main.rb
@@ -0,0 +1,18 @@
+require "./tictactoe.rb"
+
+hash = {
+  0 => "Choose again",
+  1 => " [1] ",
+  2 => " [2] ",
+  3 => " [3] ",
+  4 => " [4] ",
+  5 => " [5] ",
+  6 => " [6] ",
+  7 => " [7] ",
+  8 => " [8] ",
+  9 => " [9] ",
+}
+
+tictactoe = Tictactoe.new(hash)
+puts "* * * Let's play Tic-Tac-Toe! * * *"
+tictactoe.game
--- tictactoe.rb
+++ tictactoe.rb
@@ -85,20 +85,3 @@ class Tictactoe
     end
   end
 end
-
-hash = {
-  0 => "Choose again",
-  1 => " [1] ",
-  2 => " [2] ",
-  3 => " [3] ",
-  4 => " [4] ",
-  5 => " [5] ",
-  6 => " [6] ",
-  7 => " [7] ",
-  8 => " [8] ",
-  9 => " [9] ",
-}
-
-tictactoe = Tictactoe.new(hash)
-puts "* * * Let's play Tic-Tac-Toe! * * *"
-tictactoe.game
